f56ade27b9da3a348c9569312b01d1a78397d7b6
Kate <katemhagen@gmail.com>
2024-10-14 18:23:41 -0700

    added icons

--- app/TabNavigator.jsx
+++ app/TabNavigator.jsx
@@ -5,7 +5,9 @@ import ScanTextScreen from './ScanTextScreen'; // Adjust path as needed
 import ChangeLanguageScreen from './ChangeLanguageScreen'; // Adjust path as needed
 import SettingsScreen from './SettingsScreen'; // Adjust path as needed
 import * as Speech from 'expo-speech'
-
+import AntDesign from '@expo/vector-icons/AntDesign';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Feather from '@expo/vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
 
@@ -20,36 +22,62 @@ const TabNavigator = () => {
       screenOptions={{
         tabBarStyle: { 
           backgroundColor: '#161622',
+          height: 100,
+          paddingTop: 10,
         },
         tabBarActiveTintColor: '#E7F1A8',
         tabBarInactiveTintColor: '#FFFDF5',
-      
       }}
     
     >
       
       <Tab.Screen 
         name="Object Recognition" 
-        options={{ headerShown: false }} 
+        options={{ 
+          headerShown: false,
+          tabBarLabel: 'Object Recognition',
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="scan1" size='32px' color={color} />
+          )
+        }} 
         component={ObjectRecognitionScreen} 
         listeners={{tabPress: () => {speak("Object Recognition")}}} 
       />
       <Tab.Screen 
         name="Scan Text" 
-        options={{ headerShown: false }} 
+        options={{ 
+          headerShown: false,
+          tabBarLabel: 'Scan Text',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="text-fields" size='32px' color={color} />
+          )
+        }} 
         component={ScanTextScreen} 
         listeners={{tabPress: () => {speak("Scan Text")}}}
       />
       <Tab.Screen 
         name="GPS Navigation" 
-        options={{ headerShown: false }} 
+        options={{ 
+          headerShown: false,
+          tabBarLabel: 'GPS Navigation',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="assistant-navigation" size='32px' color={color} />
+          )
+        }} 
         component={ChangeLanguageScreen} 
         listeners={{tabPress: () => {speak("GPS Navigation")}}}  
       />
       {/* Change Language and disable text to speech for changing tabs could be in settings */}
       <Tab.Screen 
         name="Settings" 
-        options={{ headerShown: false }} 
+        options={{ 
+          headerShown: false,
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color }) => (
+            <Feather name="settings" size='32px' color={color} />
+          ),
+          
+        }} 
         component={SettingsScreen} 
         listeners={{tabPress: () => {speak("Settings")}}}
       />
