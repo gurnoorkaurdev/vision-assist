ca289b2735db40b02ffa4289cf83586fc64c2ff0
mgnegrete <mgnegrete24@gmail.com>
2024-10-23 23:35:02 -0700

    added gps implementation

---
+++ backend/gps/MapView.js
@@ -0,0 +1,9 @@
+// PlatformMap.js
+import { Platform } from 'react-native';
+import { MapComponent } from './AppleMapView';
+import { MapKitComponent } from './MapKitView';
+
+export const PlatformMap = Platform.select({
+    ios: MapComponent,
+    android: MapKitComponent,
+});
