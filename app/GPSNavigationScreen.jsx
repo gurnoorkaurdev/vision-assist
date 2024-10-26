de0fe666b6acd71dd5d78d898296304b140d329b
mgnegrete <mgnegrete24@gmail.com>
2024-10-25 22:38:49 -0700

    added gps function

--- app/GPSNavigationScreen.jsx
+++ app/GPSNavigationScreen.jsx
@@ -1,23 +1,34 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, Text, StyleSheet, View } from "react-native";
+import { PlatformMap } from '../backend/gps/MapView';
 
 const GPSNavigationScreen = () => {
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <Text style={styles.text}>GPS Navigation</Text>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}>GPS Navigation</Text>
+            </View>
+            <View style={styles.contentContainer}>
+                <PlatformMap />
+            </View>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-  },
+    container: {
+        flex: 1,
+    },
+    headerContainer: {
+        padding: 16,
+        alignItems: 'center',
+    },
+    headerText: {
+        fontSize: 24,
+    },
+    contentContainer: {
+        flex: 1,
+    },
 });
 
 export default GPSNavigationScreen;
